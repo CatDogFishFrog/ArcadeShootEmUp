302cb7f21026715dfe09732608702ed1719d5e5f
VadimDadim <vadim.goncharov.1999@gmail.com>
2018-05-26 00:45:45 +0300

    Загружен код и Контент проекта

---
+++ Source/ArcadeShootEmUp/ArcadeShootEmUp.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ArcadeShootEmUp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArcadeShootEmUp, "ArcadeShootEmUp" );
---
+++ Source/ArcadeShootEmUp/ArcadeShootEmUpGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArcadeShootEmUpGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCADESHOOTEMUP_API AArcadeShootEmUpGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
